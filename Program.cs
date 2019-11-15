7bc528a51688a865a1601c26a5c89305ffb6360c
sethoak <sethoak@gmail.com>
2019-11-15 15:08:21 -0600

    saving files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_English_Idioms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
